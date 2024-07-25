f052a791359a1d162b656e9935129b7883240059
Thiraj Pramodya <thirajpramodya@Thirajs-MacBook-Pro.local>
2024-07-25 15:15:53 +0530

    recipe instructions end point set

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -24,16 +24,30 @@ export default function RecipeDetails({ recipeId }) {
       Recipe Details {recipeId}
       <div className={styles.recipecontainer}>
         <h1 className={styles.recipetitle}>{recipeDetails.title}</h1>
-        <div className={styles.imageContainer}>
-          <img src={recipeDetails.image} alt="" />
+        <img className={styles.recipeImage} src={recipeDetails.image} alt="" />
+
+        <div className={styles.recipeInstructions}>
+          <div>
+            <span className={styles.preperationTitle}>
+              Preparation Time: {recipeDetails.readyInMinutes} minutes
+            </span>
+            <span className={styles.servingTitle}>
+              Servings : {recipeDetails.servings}
+            </span>
+            <span className={styles.vegitarianTitle}>
+              Vegitarian :{" "}
+              {recipeDetails.vegetarian ? "Vegetarian" : "Non- Vegetarian"}
+            </span>
+          </div>
+          <div className={styles.instructionsContainer}>
+            <span className={styles.instructionsTitle}>Instructions</span>
+            {isLoading
+              ? "Loading..."
+              : recipeDetails.analyzedInstructions[0].steps.map((step) => (
+                  <li>{step.step}</li>
+                ))}
+          </div>
         </div>
-        <span>
-          {isLoading
-            ? "Loading..."
-            : recipeDetails.analyzedInstructions[0].steps.map((step) => (
-                <li>{step.step}</li>
-              ))}
-        </span>
       </div>
     </div>
   );
